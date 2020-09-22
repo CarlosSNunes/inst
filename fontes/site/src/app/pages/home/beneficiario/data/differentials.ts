5f55f50b8f1da23f77b69ee9655027f2aaa240c7
neotix-alexandre-ishikawa <alexandre.ishikawa@neotix.com.br>
2020-09-22 19:20:41 -0300

    Troca imagenns e texto diferencial beneficiario, adicao de campo model banner e ajuste nos textos do bloco rede plus

--- fontes/site/src/app/pages/home/beneficiario/data/differentials.ts
+++ fontes/site/src/app/pages/home/beneficiario/data/differentials.ts
@@ -5,28 +5,28 @@ export const differentialsMock = [
         title: "Consulta Garantida",
         content: "m alguns planos e regiões, a Care Plus garante aos beneficiários consultas de determinadas especialidades em até 72 horas",
         active: false,
-        image: "assets/img/acesso-a-ferramentas-digitais.jpg",
+        image: "assets/img/diferenciais-beneficiario-consulta-garantida.png",
         time: 10000
     }),
     new DifferentialModel({
         title: "Care Plus Travel",
         content: "Nossa proteção de até US$ 300.000,00 garante atendimento aos beneficiários que estão em viagem pelo exterior.",
         active: false,
-        image: "assets/img/consultas-e-exames.jpg",
+        image: "assets/img/diferenciais-beneficiario-travel.png",
         time: 10000
     }),
     new DifferentialModel({
         title: "Saúde em Casa",
         content: "A Care Plus proporciona aos seus beneficiários consultas domiciliares nas especialidades de pediatria e clínica geral.",
         active: false,
-        image: "assets/img/comprometimento-com-prazos.jpg",
+        image: "assets/img/diferenciais-beneficiario-saude-em-casa.png",
         time: 10000
     }),
     new DifferentialModel({
         title: "Coleta de Exames",
         content: "Os beneficiários podem solicitar a coleta domiciliar de material para exames laboratoriais em domicílio.",
         active: true,
-        image: "assets/img/vantagens-excepcionais.jpg",
+        image: "assets/img/diferenciais-beneficiario-coleta.png",
         time: 10000
     }),
 ];
