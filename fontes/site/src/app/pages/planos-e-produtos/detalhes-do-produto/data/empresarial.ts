cd6e99fc5b6421a8f6980f739d0235551be32be8
neotix-matheuscampos <matheus.campos@neotix.com.br>
2020-10-15 18:53:29 -0300

    Ajuste nos cards para gestão de saúde no plano Care Plus Empresarial

--- fontes/site/src/app/pages/planos-e-produtos/detalhes-do-produto/data/empresarial.ts
+++ fontes/site/src/app/pages/planos-e-produtos/detalhes-do-produto/data/empresarial.ts
@@ -351,14 +351,14 @@ export default {
                 "title": "Personal System",
                 "description": "A Care Plus oferece consultas em clínicas próprias com médicos, nutricionistas e psicólogos. Conheça os programas.",
                 "imagePath": "assets/svg/doctor.svg",
-                "routerLink": "/planos-e-produtos/gestao-de-saude/gestao-de-saude/personal-system"
+                "routerLink": "/planos-e-produtos/gestao-de-saude/personal-system"
             }),
             new IconCardModel({
                 "type": "icon",
                 "title": "Programas Empresariais",
                 "description": "A Care Plus disponibiliza  médicos, nutricionistas, enfermeiras e psicólogos para realizar atendimento no ambiente das empresas.",
                 "imagePath": "assets/svg/doctorphone.svg",
-                "routerLink": "/planos-e-produtos/gestao-de-saude/gestao-de-saude/programas-empresariais"
+                "routerLink": "/planos-e-produtos/gestao-de-saude/programas-empresariais"
             })
         ],
         columnClass: 'is-3-desktop'
