0bf0f5297530850dad4ec60367d3b861592daf4c
Hugo Felipe <hugo.silva@bs2.com>
2020-11-09 16:59:15 -0300

    Inclusao das variaveis de ambiente para a service do Dashboard

--- fontes/cms/src/app/neocms/dashboard/dashboard.service.ts
+++ fontes/cms/src/app/neocms/dashboard/dashboard.service.ts
@@ -2,32 +2,34 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 
+import { environment } from '../../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  private url = 'http://localhost:8081/Dashboard/';
+  private readonly API_ENDPOINT = environment.API + '/Dashboard/';
 
   constructor(
     private http: HttpClient
   ) { }
 
   getUsuariosAtivos() {
-    return this.http.get<string>(this.url + 'usuario-ativo');
+    return this.http.get<string>(this.API_ENDPOINT + 'usuario-ativo');
   }
 
   getBannerAtivos() {
-    return this.http.get<string>(this.url + 'banner-ativo') ;
+    return this.http.get<string>(this.API_ENDPOINT + 'banner-ativo') ;
   }
 
   getPostsAtivos() {
-    return this.http.get<string>(this.url  + 'post-ativo');
+    return this.http.get<string>(this.API_ENDPOINT  + 'post-ativo');
   }
 
   getPostsMaisLidos()
   {
-    return this.http.get<any[]>(this.url + 'posts-mais-lidos');
+    return this.http.get<any[]>(this.API_ENDPOINT + 'posts-mais-lidos');
   }
 
   errorHandler(error: HttpErrorResponse) {
