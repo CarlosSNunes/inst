bcad1fa5e32cbbc30e02393f7a69ef8fbcdd9c0d
João Lucas <joao.lucas@neotix.com.br>
2020-11-27 14:25:21 -0300

    ajuste do environment de prod

--- fontes/cms/src/environments/environment.prod.ts
+++ fontes/cms/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: 'https://localhost:4000',
+  API: 'http://52.3.44.106:8081',
   INSTITUCIONAL_URL: 'http://careplus.homolog.neotix.com.br'
 };
