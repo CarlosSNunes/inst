6d6b4870bca7cf47a968ec3516b1f87e600a619a
neotix-matheuscampos <matheus.campos@neotix.com.br>
2021-01-12 11:36:27 -0300

    Adicionando variavel de ambiente staging-cp em todos os projetos

---
+++ fontes/cms/src/environments/environment.staging-cp.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    API: 'http://uat7institucional.careplus.com.br/apiinstitucional/',
+    INSTITUCIONAL_URL: 'http://uat7institucional.careplus.com.br/institucional/',
+};
---
+++ fontes/site/src/environments/environment.staging-cp.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: true,
+    API_URL: 'http://uat7institucional.careplus.com.br/apiinstitucional',
+    SELF_URL: 'http://uat7institucional.careplus.com.br/institucional/',
+    CAREPLUS_URL: 'https://hml.careplus.com.br/homolog23/',
+    BASE_HREF: '/',
+    API_USER: '1NJugPtxBoUY+a2s6jGMbA==',
+    API_PASSWORD: 'BH/+XmrBem4SzvEs2xfjAQ==',
+    HAS_SSL_CERTIFIED: true,
+    CERT_PATH: {
+        key: 'E:/certs/uat7institucional.key',
+        cert: 'E:/certs/uat7institucional.crt',
+        ca: 'E:/certs/uat7institucional.cer'
+    }
+};
