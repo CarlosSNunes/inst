df0735e75a6f5ec4a44c916675c264df5d019790
João Lucas <joao.lucas@neotix.com.br>
2021-03-16 16:20:18 -0300

    troca da rota de autenticação do site

--- fontes/site/src/environments/environment.staging-cp.ts
+++ fontes/site/src/environments/environment.staging-cp.ts
@@ -4,8 +4,8 @@ export const environment = {
     SELF_URL: 'http://uat7institucional.careplus.com.br',
     CAREPLUS_URL: 'https://qa.careplus.com.br/portal/',
     BASE_HREF: '/',
-    API_USER: 'DAAAAOG4eBusq6vSwpxTSRAAAAC/VRFflq3SmPIWk9f9Iy4I5F4V7dGJlvkF1g==',
-    API_PASSWORD: 'DAAAAMnWjqRWpoBJoZTNIhAAAAAO9vT6eiNhlYZsfdYoDAcXYqwPW1ESh56/FDcD',
+    API_USER: 'DAAAACcfrLxDBCdWfUs5KBAAAAD7eEXzPpaPiKpIg9DEMkyFzgrU4Bjf',
+    API_PASSWORD: 'DAAAAOrbz0oi9UpgKGqFORAAAADunM+kc616EetW5ShUDX10MFiHH2ekVK/VZVCMfv5dMO0=',
     HAS_SSL_CERTIFIED: true,
     CERT_PATH: {
         key: 'E:/certs/uat7institucional.key',
