99b67e95a14d151f492b721c3d6ee6ab10bbf454
João Lucas <joao.lucas@neotix.com.br>
2021-04-13 10:23:18 -0300

    troca da ordem dos documentos

--- fontes/site/src/app/pages/pesquisa-satisfacao/data/mock.ts
+++ fontes/site/src/app/pages/pesquisa-satisfacao/data/mock.ts
@@ -21,28 +21,6 @@ export const simpleBannerModel: SimpleBannerModel = new SimpleBannerModel({
 export const materiais = [
     new CardModel({
         type: 'default',
-        title: 'Pesquisa de Satisfação - 2020 (base 2019)',
-        button: new ButtonModel({
-            link: 'assets/documents/pesquisa_de_satisfacao_2020_base_2019.pdf'
-        }),
-        size: {
-            placeholder: '(2.26mb)',
-            file_size: '2264863'
-        }
-    }),
-    new CardModel({
-        type: 'default',
-        title: 'Parecer de Auditoria - Pesquisa de Satisfação - 2020 (base 2019)',
-        button: new ButtonModel({
-            link: 'assets/documents/parecer_de_auditoria_pesquisa_de_satisfacao-2020_base_2019.pdf'
-        }),
-        size: {
-            placeholder: '(249kb)',
-            file_size: '254982'
-        }
-    }),
-    new CardModel({
-        type: 'default',
         title: 'Pesquisa de Satisfação - 2021 (Base 2020)',
         button: new ButtonModel({
             link: 'assets/documents/pesquisa_de_satisfacao-2021_base_2020.pdf'
@@ -63,4 +41,27 @@ export const materiais = [
             file_size: '118734'
         }
     }),
+    new CardModel({
+        type: 'default',
+        title: 'Pesquisa de Satisfação - 2020 (base 2019)',
+        button: new ButtonModel({
+            link: 'assets/documents/pesquisa_de_satisfacao_2020_base_2019.pdf'
+        }),
+        size: {
+            placeholder: '(2.26mb)',
+            file_size: '2264863'
+        }
+    }),
+    new CardModel({
+        type: 'default',
+        title: 'Parecer de Auditoria - Pesquisa de Satisfação - 2020 (base 2019)',
+        button: new ButtonModel({
+            link: 'assets/documents/parecer_de_auditoria_pesquisa_de_satisfacao-2020_base_2019.pdf'
+        }),
+        size: {
+            placeholder: '(249kb)',
+            file_size: '254982'
+        }
+    }),
+   
 ]
