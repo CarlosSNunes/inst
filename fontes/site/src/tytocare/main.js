47ade8a1c7ab5ab4ec1493dce720e1ee8fc694e1
neotix-marcioteixeira <marcio.teixeira@neotix.com.br>
2021-07-26 18:58:29 -0300

    ajustes finais tytocare

--- fontes/site/src/tytocare/main.js
+++ fontes/site/src/tytocare/main.js
@@ -19,7 +19,7 @@ $((function(){
     $(".scroll").on('click',(function (event) {
         event.preventDefault();
         $('html,body').animate({
-            scrollTop: $(this.hash).offset().top
+            scrollTop: $(this.hash).offset().top - 100
         }, 150);
     }));
 
