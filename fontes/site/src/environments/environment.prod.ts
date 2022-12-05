54c3e4b40b3e45ecc0725a8271797769ea6db857
luizandrealves <lalves@careplus.com.br>
2022-12-05 14:17:05 -0300

    Update environment.prod.ts

--- fontes/site/src/environments/environment.prod.ts
+++ fontes/site/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
     CDN_URL: 'https://cdn.careplus.com.br/gerinstitucional',
     CAREPLUS_URL: 'https://www8.careplus.com.br/portal/',
     BASE_HREF: '/',
-    API_USER: 'DAAAACcfrLxDBCdWfUs5KBAAAAD7eEXzPpaPiKpIg9DEMkyFzgrU4Bjf',
-    API_PASSWORD: 'DAAAAOrbz0oi9UpgKGqFORAAAADunM+kc616EetW5ShUDX10MFiHH2ekVK/VZVCMfv5dMO0=',
+    API_USER: '#{API_USER}#',
+    API_PASSWORD: '#{API_PASS}#',
     HAS_SSL_CERTIFIED: true,
     CERT_PATH: {
         key: 'E:/certs/www.careplus.com.br.key',
